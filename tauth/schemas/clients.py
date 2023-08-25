92955a000f9e2f5caa59a59c733a651e0cc9627c
Nei Cardoso de Oliveira Neto <nei.neto@hotmail.com>
2023-08-25 18:04:59 -0300

    Create workflow that runs tests

--- tauth/schemas/clients.py
+++ tauth/schemas/clients.py
@@ -2,7 +2,12 @@ from datetime import datetime
 
 from pydantic import BaseModel, EmailStr, Field
 
-from melting_schemas.meta import Creator
+
+class Creator(BaseModel):
+    client_name: str
+    token_name: str
+    user_email: EmailStr
+    user_ip: str = "127.0.0.1"
 
 
 class UserCreation(BaseModel):
