4681381aa5a00827ee61868a22ebba88147fe3a1
Joao Severo <joaovbsevero@gmail.com>
2023-08-29 11:51:03 -0300

    Fix field type

--- tauth/dependencies/security.py
+++ tauth/dependencies/security.py
@@ -4,6 +4,7 @@ from typing import Iterable
 from fastapi import Depends, FastAPI, Header, HTTPException, Request, Security
 from fastapi.security.http import HTTPAuthorizationCredentials, HTTPBase
 from http_error_schemas.schemas import RequestValidationError
+from pydantic import EmailStr
 
 from ..auth import auth0, azure_jwt, teia_api_key
 from ..settings import Settings
@@ -28,7 +29,7 @@ class RequestAuthenticator:
     @staticmethod
     def validate(
         request: Request,
-        user_email: str | None = Header(default=None, alias="X-User-Email", description="Ignore when using OAuth."),
+        user_email: EmailStr | None = Header(default=None, alias="X-User-Email", description="Ignore when using OAuth."),
         id_token : str | None = Header(default=None, alias="X-ID-Token", description="Auth0 ID token."),
         authorization: HTTPAuthorizationCredentials | None = Security(HTTPBase(scheme="bearer", auto_error=False)),
     ):
@@ -36,6 +37,7 @@ class RequestAuthenticator:
         req_path: str = request.scope["path"]
         if request.method == "GET" and req_path in RequestAuthenticator.ignore_paths:
             return
+        
         if not authorization:
             d = RequestValidationError(
                 loc=["header", "Authorization"],
@@ -43,19 +45,24 @@ class RequestAuthenticator:
                 type="MissingHeader",
             )
             raise HTTPException(401, detail=d)
+        
         token_type, token_value = authorization.scheme, authorization.credentials
         if token_type.lower() != "bearer":
             raise HTTPException(401, detail={"msg": "Invalid authorization scheme; expected 'bearer'."})
+        
         if token_value.startswith("MELT_"):
             log.debug("Authenticating with a TEIA API key.")
             teia_api_key.RequestAuthenticator.validate(request, user_email, token_value)
             return
+        
         if Settings.get().ENABLE_AUTH0 and id_token is not None:
             log.debug("Authenticating with Auth0.")
             auth0.RequestAuthenticator.validate(request, token_value, id_token)
             return
+        
         if Settings.get().ENABLE_AZURE:
             log.debug("Authenticating with an Azure JWT.")
             azure_jwt.RequestAuthenticator.validate(request, token_value)
             return
+        
         raise HTTPException(401, detail={"msg": "No authentication method succeeded."})
