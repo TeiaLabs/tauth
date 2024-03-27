4d41e1f72eb14e22a21b2fe725e5bde1eb6defe3
Martin Duarte More <martinduartemore@gmail.com>
2024-03-27 15:35:07 -0300

    Generalize tauth to support another dynamic auth0
    Co-authored-by: Nei Cardoso de Oliveira Neto <nei.neto@hotmail.com>

--- tauth/auth/auth0_dyn.py
+++ tauth/auth/auth0_dyn.py
@@ -23,6 +23,9 @@ class Auth0Settings(BaseSettings):
     AUTH2_DOMAIN: str = ""
     AUTH2_AUDIENCE: str = ""
 
+    AUTH3_DOMAIN: str = ""
+    AUTH3_AUDIENCE: str = ""
+
     def validate(self):
         return True
 
@@ -31,7 +34,11 @@ class Auth0Settings(BaseSettings):
 
 
 class ManyJSONKeyStore:
-    domains: dict[str, str] = {"/allai/chat/webui": Auth0Settings().AUTH2_DOMAIN}
+    sets = Auth0Settings()
+    domains: dict[str, tuple[str, str]] = {
+        "/athena/chat/webui": (sets.AUTH3_DOMAIN, sets.AUTH3_AUDIENCE),
+        "/allai/chat/webui": (sets.AUTH2_DOMAIN, sets.AUTH2_AUDIENCE),
+    }
 
     @classmethod
     @ttl_cache(maxsize=1, ttl=60 * 60 * 6)
@@ -47,31 +54,41 @@ class ManyJSONKeyStore:
         log.info(f"JWK fetched from {domain}.")
         return JsonWebKey.import_key_set(res.json())
 
-    @classmethod
-    def get_jwks(cls) -> dict[str, KeySet]:
-        return {k: cls.get_jwk(v) for k, v in cls.domains.items()}
-
 
 class RequestAuthenticator:
     settings = Auth0Settings()
-    access_claims_options = {
-        "aud": {"essential": True, "value": settings.AUTH2_AUDIENCE},
-        "exp": {"essential": True},
-        "iss": {"essential": True, "value": f"https://{settings.AUTH2_DOMAIN}/"},
-    }
-    id_claims_options = {
-        "iss": {"essential": True, "value": f"https://{settings.AUTH2_DOMAIN}/"},
-        "exp": {"essential": True},
-    }
+
+    @staticmethod
+    def get_id_claims(domain: str) -> dict:
+        id_claims_options = {
+            "iss": {"essential": True, "value": f"https://{domain}/"},
+            "exp": {"essential": True},
+        }
+        return id_claims_options
+
+    @staticmethod
+    def get_access_claims(domain: str, audience: str) -> dict:
+        access_claims_options = {
+            "aud": {"essential": True, "value": audience},
+            "exp": {"essential": True},
+            "iss": {"essential": True, "value": f"https://{domain}/"},
+        }
+        return access_claims_options
 
     @staticmethod
     def validate(request: Request, token_value: str, id_token: str):
-        for app_name, j_w_k in ManyJSONKeyStore.get_jwks().items():
+        for i, (app_name, (domain, audience)) in enumerate(
+            ManyJSONKeyStore.domains.items()
+        ):
+            print(app_name, domain, audience)
+            j_w_k = ManyJSONKeyStore.get_jwk(domain)
             try:
                 access_claims = jwt.decode(
                     token_value,
                     j_w_k,
-                    claims_options=RequestAuthenticator.access_claims_options,
+                    claims_options=RequestAuthenticator.get_access_claims(
+                        domain, audience
+                    ),
                 )
                 access_claims.validate()
             except (
@@ -80,28 +97,32 @@ class RequestAuthenticator:
                 InvalidTokenError,
                 MissingClaimError,
             ) as e:
-                raise HTTPException(
-                    401,
-                    detail={
-                        "loc": ["headers", "Authorization"],
-                        "msg": e.description,
-                        "type": e.error,
-                    },
-                )
+                if i == len(ManyJSONKeyStore.domains) - 1:
+                    raise HTTPException(
+                        401,
+                        detail={
+                            "loc": ["headers", "Authorization"],
+                            "msg": e.description,
+                            "type": e.error,
+                        },
+                    )
+                continue
             except Exception as e:
-                raise HTTPException(
-                    401,
-                    detail={
-                        "loc": ["headers", "Authorization"],
-                        "msg": str(e),
-                        "type": type(e).__name__,
-                    },
-                )
+                if i == len(ManyJSONKeyStore.domains) - 1:
+                    raise HTTPException(
+                        401,
+                        detail={
+                            "loc": ["headers", "Authorization"],
+                            "msg": str(e),
+                            "type": type(e).__name__,
+                        },
+                    )
+                continue
             try:
                 id_claims = jwt.decode(
                     id_token,
                     j_w_k,
-                    claims_options=RequestAuthenticator.id_claims_options,
+                    claims_options=RequestAuthenticator.get_id_claims(domain),
                 )
                 id_claims.validate()
             except (
@@ -110,23 +131,27 @@ class RequestAuthenticator:
                 InvalidTokenError,
                 ExpiredTokenError,
             ) as e:
-                raise HTTPException(
-                    401,
-                    detail={
-                        "loc": ["headers", "X-ID-Token"],
-                        "msg": e.description,
-                        "type": e.error,
-                    },
-                )
+                if i == len(ManyJSONKeyStore.domains) - 1:
+                    raise HTTPException(
+                        401,
+                        detail={
+                            "loc": ["headers", "X-ID-Token"],
+                            "msg": e.description,
+                            "type": e.error,
+                        },
+                    )
+                continue
             except Exception as e:
-                raise HTTPException(
-                    401,
-                    detail={
-                        "loc": ["headers", "X-ID-Token"],
-                        "msg": str(e),
-                        "type": type(e).__name__,
-                    },
-                )
+                if i == len(ManyJSONKeyStore.domains) - 1:
+                    raise HTTPException(
+                        401,
+                        detail={
+                            "loc": ["headers", "X-ID-Token"],
+                            "msg": str(e),
+                            "type": type(e).__name__,
+                        },
+                    )
+                continue
             user_id = id_claims.get("sub")
             if not user_id:
                 raise HTTPException(
@@ -148,7 +173,7 @@ class RequestAuthenticator:
             org_id = access_claims.get("org_id")
             if not org_id:
                 raise HTTPException(401, detail="Missing 'org_id' claim.")
-            org = org_controllers.read_one("/osf--auth0-org-id", org_id)
+            org = org_controllers.read_one({"$regex": r"/.*--auth0-org-id"}, org_id)  # type: ignore
             if not org:
                 raise HTTPException(
                     401, detail=f"Organization with id '{org_id}' not found."
