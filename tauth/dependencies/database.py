8a09b3087dd03034065094a219199ba643d69dd3
Ramiro Nilson Barros <ramiro.nilson.barros@gmail.com>
2024-04-03 14:28:49 -0300

    Change tauth alias to tauth

--- tauth/dependencies/database.py
+++ tauth/dependencies/database.py
@@ -9,6 +9,7 @@ def init_app(sets: Settings):
     DB.add_conn(
         db_name=sets.TAUTH_MONGODB_DBNAME,
         uri=sets.TAUTH_MONGODB_URI,
+        alias="tauth",
     )
     for m in Document.__subclasses__():
         m.create_indexes()
