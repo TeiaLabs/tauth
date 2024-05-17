f79be6f58819a032372677283effd4931b1ccf6f
Nei Cardoso de Oliveira Neto <nei.neto@hotmail.com>
2024-05-17 20:06:03 -0300

    Add logic to the entities migration

--- scripts/migration-v1-to-v2.py
+++ scripts/migration-v1-to-v2.py
@@ -5,6 +5,12 @@ Migration script to convert data from v1 to v2.
 import argparse
 
 from pymongo import MongoClient
+from pymongo.collection import Collection
+from pymongo.database import Database
+
+from tauth.entities.models import EntityDAO, EntityRef
+from tauth.entities.schemas import EntityIntermediate
+from tauth.schemas import Creator, Infostar
 
 
 def get_args():
@@ -22,18 +28,72 @@ def get_args():
     return args
 
 
+def conditional_action(action, inaction, dry_run: bool):
+    if not dry_run:
+        action()
+    else:
+        inaction()
+
+
+def conditional_insert(collection: Collection, document: dict, dry_run: bool):
+    if not dry_run:
+        collection.insert_one(document)
+    else:
+        print(f"Would insert: '{document}' into '{collection.name}'.")
+
+
+def migrate_entities(db: Database, dry_run: bool):
+    entity_col = db[EntityDAO.collection_name()]
+    ## Organizations
+    # Create root organization entity for '/' token
+    root_org = EntityIntermediate(
+        handle="/",
+        owner_ref=None,
+        type="organization",
+    )
+    conditional_insert(entity_col, root_org.model_dump(), dry_run)
+    obj = entity_col.find_one({"handle": "/"})
+    if obj:
+        root_org_ref = EntityDAO(**obj).to_ref()
+    else:
+        root_org_ref = EntityRef(handle="/", id="mock_id", type="organization")
+    # Query 'tokens' collection for all client names
+    client_names = db["tokens"].distinct("client_name")
+    for client_name in client_names:
+        # Extract the root organization (e.g., `/teialabs/athena` -> `teialabs`)
+        root_org_handle = client_name.split("/")[1]
+        # Create organization entity based on client name
+        org = EntityIntermediate(
+            handle=f"/{root_org_handle}",
+            owner_ref=root_org_ref,
+            type="organization",
+        )
+        conditional_insert(entity_col, org.model_dump(), dry_run)
+    ## services
+    # extract the service names (/teialabs/athena -> [athena], /osf/allai/code -> [allai, code])
+    service_names = {
+        client_name.split("/")[1:]: tuple(client_name.split("/")[2:])
+        for client_name in client_names
+    }
+    for org_name, service_name in service_names:
+        org = entity_col.find_one({"handle": org_name})
+        if not org:
+            raise ValueError(f"Organization entity '{org_name}' not found.")
+        org_ref = EntityDAO(**org).to_ref()
+        # create service entities based on service names
+        service = EntityIntermediate(
+            handle=f"{'--'.join(service_name)}",
+            owner_ref=org_ref,
+            type="service",
+        )
+        conditional_insert(entity_col, service.model_dump(), dry_run)
+
+
 def main():
     args = get_args()
     client = MongoClient(args.host, args.port)
     db = client[args.in_db]
 
-    # Entities
-    ## Organizations
-    #  Create root organization entity for '/' token
-    #  Query 'tokens' collection for all client names
-    #  For each client name
-    #    Extract the root organization (e.g., `/teialabs/athena` -> `/teialabs`)
-    #    Create organization entity based on client name
     ## Users
     #  Query 'tokens' collection for all (client_name, user_email) pairs
     #  For each pair
