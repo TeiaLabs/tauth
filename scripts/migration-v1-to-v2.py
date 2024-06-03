0841877e4661310bfc3044c302aeb3cf6ab4fe38
Nei Cardoso de Oliveira Neto <nei.neto@hotmail.com>
2024-06-03 16:01:19 -0300

    Add final polish to migration script
    Co-authored-by: Martin Duarte More <martinduartemore@gmail.com>

--- scripts/migration-v1-to-v2.py
+++ scripts/migration-v1-to-v2.py
@@ -3,35 +3,54 @@ Migration script to convert data from v1 to v2.
 """
 
 import argparse
+from collections import defaultdict
 from typing import Callable, Literal
 
-from pymongo import MongoClient
+from loguru import logger
+from pymongo import MongoClient, errors
 from pymongo.collection import Collection
 from pymongo.database import Database
 from redbaby.pyobjectid import PyObjectId
 from tqdm import tqdm
 
 from tauth.authproviders.models import AuthProviderDAO
-from tauth.entities.models import EntityDAO, EntityRef
-from tauth.entities.schemas import EntityIntermediate
+from tauth.entities.models import EntityDAO, EntityRef, OrganizationRef, ServiceRef
 from tauth.legacy.tokens import TokenDAO
-from tauth.schemas import Creator, Infostar
+from tauth.schemas import Infostar
 from tauth.schemas.attribute import Attribute
 
 OID = PyObjectId()
 MY_IP = "127.0.0.1"
 MY_EMAIL = "sysadmin@teialabs.com"
 
+SYSTEM_INFOSTAR = Infostar(
+    request_id=OID,
+    apikey_name="migrations",
+    authprovider_org="/",
+    authprovider_type="melt-key",
+    extra={},
+    service_handle="tauth",
+    user_handle=MY_EMAIL,
+    user_owner_handle="/",
+    original=None,
+    client_ip=MY_IP,
+)
+
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--host", type=str, required=True, help="MongoDB host.")
+    parser.add_argument(
+        "--host",
+        type=str,
+        default="mongodb://localhost:27017",
+        help="MongoDB host.",
+    )
     parser.add_argument("--port", type=int, default="27017", help="MongoDB port.")
     parser.add_argument(
-        "--in_db", type=str, default="tauth", help="MongoDB input database."
+        "--in-db", type=str, default="tauth", help="MongoDB input database."
     )
     parser.add_argument(
-        "--out_db", type=str, default="tauth-v2", help="MongoDB output database."
+        "--out-db", type=str, default="tauth-v2", help="MongoDB output database."
     )
     parser.add_argument("--dry-run", action="store_true", help="Dry run.")
     args = parser.parse_args()
@@ -45,7 +64,7 @@ class Partial:
         self.kwargs = kwargs
 
     def __call__(self, *args, **kwargs):
-        return self.func(*self.args, *args, **self.kwargs, **kwargs)
+        return self.func(*self.args, **self.kwargs, **kwargs)
 
     def __repr__(self) -> str:
         return f"Partial({self.func!r}, {self.args!r}, {self.kwargs!r})"
@@ -55,53 +74,70 @@ class Partial:
 
 
 def conditional_action(
-    action: Partial, dry_run: bool, inaction: Callable[[Partial]] = print
+    action: Partial, dry_run: bool, inaction: Callable[[Partial], None] = logger.info
 ):
     if not dry_run:
+        logger.debug(f"Executing action: {action}")
         action()
     else:
+        logger.debug(f"Would execute: {action}")
         inaction(action)
 
 
 def conditional_insert(collection: Collection, document: dict, dry_run: bool):
     if not dry_run:
+        logger.info(f"Inserting document into {collection.name}: {document}")
         collection.insert_one(document)
     else:
-        print(f"Would insert: '{document}' into '{collection.name}'.")
+        logger.info(f"Would insert: '{document}' into '{collection.name}'.")
 
 
-def migrate_orgs(db: Database, dry_run: bool):
-    entity_col = db[EntityDAO.collection_name()]
+def migrate_orgs(
+    client: MongoClient, old_db_name: str, new_db_name: str, dry_run: bool
+):
+    old_db = client[old_db_name]
+    new_db = client[new_db_name]
+    new_entity_col = new_db[EntityDAO.collection_name()]
+    new_entity_col.create_indexes(EntityDAO.indexes())
     # Create root organization entity for '/' token
-    root_org = EntityIntermediate(
+    root_org = EntityDAO(
+        created_by=SYSTEM_INFOSTAR,
         handle="/",
         owner_ref=None,
         type="organization",
     )
-    conditional_insert(entity_col, root_org.model_dump(), dry_run)
-    obj = entity_col.find_one({"handle": "/"})
+    try:
+        conditional_insert(new_entity_col, root_org.bson(), dry_run)
+    except errors.DuplicateKeyError:
+        pass
+    obj = new_entity_col.find_one({"handle": "/"})
     if obj:
         root_org_ref = EntityDAO(**obj).to_ref()
     else:
-        root_org_ref = EntityRef(handle="/", id="mock_id", type="organization")
+        root_org_ref = EntityRef(handle="/", type="organization")
     # Query 'tokens' collection for all client names
-    client_names = db["tokens"].distinct("client_name")
+    client_names = old_db["tokens"].distinct("client_name")
     for client_name in client_names:
+        logger.debug(f"Creating organization entity for '{client_name}'")
         # Extract the root organization (e.g., `/teialabs/athena` -> `teialabs`)
         root_org_handle = client_name.split("/")[1]
         # Create organization entity based on client name
-        org = EntityIntermediate(
+        org = EntityDAO(
+            created_by=SYSTEM_INFOSTAR,
             handle=f"/{root_org_handle}",
             owner_ref=root_org_ref,
             type="organization",
         )
-        conditional_insert(entity_col, org.model_dump(), dry_run)
+        try:
+            conditional_insert(new_entity_col, org.bson(), dry_run)
+        except errors.DuplicateKeyError:
+            logger.warning(f"Organization entity '{org.handle}' already exists.")
     # query 'organizations' collection for all auth0 org-ids (/teialabs--auth0-org-id)
-    orgs = db["organizations"].find()
+    orgs = old_db["organizations"].find()
     for org in orgs:
         conditional_action(
             Partial(
-                entity_col.update_one,
+                new_entity_col.update_one,
                 {"handle": org["name"]},
                 {"$set": {"external_ids": org["external_ids"]}},
             ),
@@ -109,41 +145,55 @@ def migrate_orgs(db: Database, dry_run: bool):
         )
 
 
-def migrate_services(db: Database, dry_run: bool):
-    entity_col = db[EntityDAO.collection_name()]
-    client_names = db["tokens"].distinct("client_name")
-    # extract the service names (/teialabs/athena -> [athena], /osf/allai/code -> [allai, code])
-    service_names = {
-        client_name.split("/")[1:]: tuple(client_name.split("/")[2:])
-        for client_name in client_names
-    }
-    for org_name, service_name in service_names:
-        org = entity_col.find_one({"handle": org_name})
-        if not org:
-            raise ValueError(f"Organization entity '{org_name}' not found.")
-        org_ref = EntityDAO(**org).to_ref()
-        # create service entities based on service names
-        service = EntityIntermediate(
-            handle=f"{'--'.join(service_name)}",
-            owner_ref=org_ref,
-            type="service",
-        )
-        conditional_insert(entity_col, service.model_dump(), dry_run)
+def migrate_services(
+    client: MongoClient, old_db_name: str, new_db_name: str, dry_run: bool
+):
+    old_db = client[old_db_name]
+    new_db = client[new_db_name]
+    entity_col_new = new_db[EntityDAO.collection_name()]
+    client_names = old_db["tokens"].distinct("client_name")
+    # extract the service names (/teialabs/athena -> [athena], /osf/allai/code -> [allai, code]
+    print(client_names)
+    service_names = defaultdict(list)
+    for client_name in client_names:
+        org_name, *service_name = client_name.split("/")[1:]
+        if service_name:
+            service_names[f"/{org_name}"].append(service_name)
+    for org_name, service_names in service_names.items():
+        for serv_name in service_names:
+            logger.debug(f"Creating service entity for '{org_name}' and '{serv_name}'")
+            org = entity_col_new.find_one({"handle": org_name})
+            if not org:
+                raise ValueError(f"Organization entity '{org_name}' not found.")
+            org_ref = EntityDAO(**org).to_ref()
+            # create service entities based on service names
+            service = EntityDAO(
+                created_by=SYSTEM_INFOSTAR,
+                handle=f"{'--'.join(serv_name)}",
+                owner_ref=org_ref,
+                type="service",
+            )
+            try:
+                conditional_insert(entity_col_new, service.bson(), dry_run)
+            except errors.DuplicateKeyError:
+                pass
 
 
-def migrate_users(db: Database, dry_run: bool):
-    entity_col = db[EntityDAO.collection_name()]
-    # query 'users' collection
-    users = db["users"].find()
+def migrate_users(
+    client: MongoClient, old_db_name: str, new_db_name: str, dry_run: bool
+):
+    old_db = client[old_db_name]
+    new_db = client[new_db_name]
+    entity_col_new = new_db[EntityDAO.collection_name()]
+    users = old_db["users"].find()
     for user in users:
-        # extract the organization (e.g., `/teialabs/athena` -> `/teialabs`)
-        # TODO: what if client_name == / ?
+        if "email" not in user:
+            continue
         org_name = user["client_name"].split("/")[1]
-        org = entity_col.find_one({"handle": org_name})
+        org = entity_col_new.find_one({"handle": f"/{org_name}"})
         if not org:
             raise ValueError(f"Organization entity '{org_name}' not found.")
         org_ref = EntityDAO(**org).to_ref()
-        # create user entity as child of organization
         user_entity = EntityDAO(
             handle=user["email"],
             owner_ref=org_ref,
@@ -151,24 +201,26 @@ def migrate_users(db: Database, dry_run: bool):
             extra=[
                 Attribute(name="melt_key_client", value=user["client_name"]),
                 Attribute(name="melt_key_client_first_login", value=user["created_at"]),
-                Attribute(name="client_ip", value=user["created_by"]["user_ip"]),
             ],
             created_by=Infostar(
                 request_id=OID,
-                apikey_name="migrations",
-                authprovider_org="/",
+                apikey_name=user["created_by"]["token_name"],
+                authprovider_org=org["handle"],
                 authprovider_type="melt-key",
                 extra={},
                 service_handle="tauth",
-                user_handle=MY_EMAIL,
-                user_owner_handle="/",
-                original=None,
-                client_ip=MY_IP,
+                user_handle=user["created_by"]["user_email"],
+                user_owner_handle=org["handle"],
+                original=SYSTEM_INFOSTAR,
+                client_ip=user["created_by"].get("user_ip", "127.0.0.1"),
             ),
         )
-        conditional_action(
-            Partial(entity_col.insert_one, user_entity.model_dump()), dry_run
-        )
+        try:
+            conditional_action(
+                Partial(entity_col_new.insert_one, user_entity.bson()), dry_run
+            )
+        except errors.DuplicateKeyError:
+            logger.warning(f"User entity '{user_entity.handle}' already exists.")
 
 
 def get_entity_ref_from_client_name(
@@ -187,105 +239,117 @@ def get_entity_ref_from_client_name(
     return EntityDAO(**entity).to_ref()
 
 
-def migrate_melt_keys(db: Database, dry_run: bool):
-    melt_keys = db[TokenDAO.collection_name()]
-    tokens = list(db["tokens"].find())
+def migrate_melt_keys(
+    client: MongoClient, old_db_name: str, new_db_name: str, dry_run: bool
+):
+    old_db = client[old_db_name]
+    new_db = client[new_db_name]
+    melt_keys = new_db[TokenDAO.collection_name()]
+    tokens = list(old_db["tokens"].find())
     for t in tqdm(tokens):
-        org = get_entity_ref_from_client_name(db, t["client_name"], "organization")
-        service = get_entity_ref_from_client_name(db, t["client_name"], "service")
-        key = {
-            "client_name": t["client_name"],
-            "name": t["name"],
-            "value": t["value"],
-            "created_by": Infostar(
+        logger.debug(f"Creating melt-key for '{t['client_name']}'")
+        org = get_entity_ref_from_client_name(new_db, t["client_name"], "organization")
+        try:
+            service_handle = get_entity_ref_from_client_name(
+                new_db, t["client_name"], "service"
+            ).handle
+        except ValueError:
+            service_handle = "tauth--api"
+        key = TokenDAO(
+            client_name=t["client_name"],
+            name=t["name"],
+            value=t["value"],
+            created_by=Infostar(
                 request_id=OID,
                 apikey_name=t["created_by"]["token_name"],
-                authprovider_org="/",
+                authprovider_org=org.handle,
                 authprovider_type="melt-key",
                 extra={},
-                service_handle=service.handle,
-                user_handle=t["email"],
+                service_handle=service_handle,
+                user_handle=t["created_by"]["user_email"],
                 user_owner_handle=org.handle,
-                original=None,
-                client_ip=t["created_by"]["user_ip"],
+                original=SYSTEM_INFOSTAR,
+                client_ip=t["created_by"].get("user_ip", "127.0.0.1"),
             ),
-        }
-        conditional_insert(melt_keys, key, dry_run)
+        )
+        try:
+            conditional_insert(melt_keys, key.bson(), dry_run)
+        except errors.DuplicateKeyError:
+            logger.warning(f"Melt-key '{key.name}' already exists.")
 
 
-def migrate_authproviders(db: Database, dry_run: bool):
-    authproviders = db[AuthProviderDAO.collection_name()]
-    infostar = Infostar(
-        request_id=OID,
-        apikey_name="migrations",
-        authprovider_org="/",
-        authprovider_type="melt-key",
-        extra={},
-        service_handle="tauth",
-        user_handle=MY_EMAIL,
-        user_owner_handle="/",
-        original=None,
-        client_ip=MY_IP,
-    )
-    # create authproviders
-    # melt-key
-    clients = db["clients"].distinct("name")
+def migrate_authproviders(
+    client: MongoClient, old_db_name: str, new_db_name: str, dry_run: bool
+):
+    old_db = client[old_db_name]
+    new_db = client[new_db_name]
+    authproviders = new_db[AuthProviderDAO.collection_name()]
+    clients = old_db["clients"].distinct("name")
     orgs = [f"/{c.split('/')[1]}" for c in clients] + ["/"]
     for org in orgs:
         authprovider = AuthProviderDAO(
-            created_by=infostar,
-            extra={},
-            organization_ref=EntityRef(handle=org, type="organization"),
+            created_by=SYSTEM_INFOSTAR,
+            organization_ref=OrganizationRef(handle=org),
             service_ref=None,
             type="melt-key",
         )
-        conditional_insert(authproviders, authprovider.model_dump(), dry_run)
-
-    # teialabs auth0
-    authprovider = AuthProviderDAO(
-        created_by=infostar,
-        external_ids=[
-            Attribute(name="issuer", value="https://dev-z60iog20x0slfn0a.us.auth0.com"),
-            Attribute(name="audience", value="api://allai.chat.webui"),
-            Attribute(name="client-id", value="4FdEO3ncOVFuROab8wf3c0GLyEMWi4f4"),
-        ],
-        extra={},
-        organization_ref=EntityRef(handle="/teialabs", type="organization"),
-        service_ref=EntityRef(handle="athena--chat", type="service"),
-        type="auth0",
-    )
-    # osf auth0 (chat)
-    authprovider = AuthProviderDAO(
-        created_by=infostar,
-        external_ids=[
-            Attribute(name="issuer", value="https://osfdigital.eu.auth0.com"),
-            Attribute(name="audience", value="api://d4f7a0b5-2a6d-476f-b251-c468a25acdef"),
-            Attribute(name="client-id", value="My4fjEfyByLfRPWzxlkP7rTDSFroklNW"),
-
-        ],
-        extra={},
-        organization_ref=EntityRef(handle="/osf", type="organization"),
-        service_ref=EntityRef(handle="allai--chat", type="service"),
-        type="auth0",
-    )
-    # osf auth0 (code)
-    authprovider = AuthProviderDAO(
-        created_by=infostar,
-        external_ids=[
-            Attribute(name="issuer", value="https://osfdigital.eu.auth0.com"),
-            Attribute(name="audience", value="???"),
-        ],
-        extra={},
-        organization_ref=EntityRef(handle="/osf", type="organization"),
-        service_ref=EntityRef(handle="allai--code", type="service"),
-        type="auth0",
-    )
+        conditional_insert(authproviders, authprovider.bson(), dry_run)
+    authproviders_data = [
+        {
+            "created_by": SYSTEM_INFOSTAR,
+            "external_ids": [
+                Attribute(
+                    name="issuer", value="https://dev-z60iog20x0slfn0a.us.auth0.com"
+                ),
+                Attribute(name="audience", value="api://allai.chat.webui"),
+                Attribute(name="client-id", value="4FdEO3ncOVFuROab8wf3c0GLyEMWi4f4"),
+            ],
+            "organization_ref": OrganizationRef(handle="/teialabs"),
+            "service_ref": ServiceRef(handle="athena--chat"),
+            "type": "auth0",
+        },
+        {
+            "created_by": SYSTEM_INFOSTAR,
+            "external_ids": [
+                Attribute(name="issuer", value="https://osfdigital.eu.auth0.com"),
+                Attribute(
+                    name="audience", value="api://d4f7a0b5-2a6d-476f-b251-c468a25acdef"
+                ),
+                Attribute(name="client-id", value="My4fjEfyByLfRPWzxlkP7rTDSFroklNW"),
+            ],
+            "organization_ref": OrganizationRef(handle="/osf"),
+            "service_ref": ServiceRef(handle="allai--chat"),
+            "type": "auth0",
+        },
+        {
+            "created_by": SYSTEM_INFOSTAR,
+            "external_ids": [
+                Attribute(name="issuer", value="https://osfdigital.eu.auth0.com"),
+                Attribute(
+                    name="audience", value="api://27a058b1-2f69-475b-b126-5d816b037cbe"
+                ),
+            ],
+            "organization_ref": OrganizationRef(handle="/osf"),
+            "service_ref": ServiceRef(handle="allai--code"),
+            "type": "auth0",
+        },
+    ]
+    for authprovider_data in authproviders_data:
+        authprovider = AuthProviderDAO(**authprovider_data)
+        conditional_insert(authproviders, authprovider.bson(), dry_run)
 
 
 def main():
     args = get_args()
+    logger.add("migration.log", level="INFO")
+    logger.info("Starting migration script")
     client = MongoClient(args.host, args.port)
-    db = client[args.in_db]
+    # migrate_orgs(client, args.in_db, args.out_db, args.dry_run)
+    # migrate_services(client, args.in_db, args.out_db, args.dry_run)
+    # migrate_users(client, args.in_db, args.out_db, args.dry_run)
+    migrate_melt_keys(client, args.in_db, args.out_db, args.dry_run)
+    migrate_authproviders(client, args.in_db, args.out_db, args.dry_run)
+    logger.info("Migration script completed")
 
 
 if __name__ == "__main__":
