2b7d34911c66f9e9a80e1bef7c8dbd018db54017
João Severo <33180595+jojo0severo@users.noreply.github.com>
2024-08-29 09:16:21 -0300

    Speed up (#14)
    * Reduce database calls, cache validation and add update as background task for melting keys
    
    * Cache validation and create user in background task

--- tauth/authn/melt_key/authentication.py
+++ tauth/authn/melt_key/authentication.py
@@ -1,44 +1,58 @@
+import contextlib
 import secrets
-from typing import Optional
+from typing import Callable, Optional
 
-from fastapi import HTTPException, Request
+from fastapi import BackgroundTasks, HTTPException, Request
 from fastapi import status as s
 from loguru import logger
 from pydantic import validate_email
+from pymongo.errors import BulkWriteError, DuplicateKeyError
 from redbaby.pyobjectid import PyObjectId
 
 from ...authproviders.models import AuthProviderDAO
-from ...authproviders.schemas import AuthProviderMoreIn
 from ...entities.models import EntityDAO, EntityRef, OrganizationRef
-from ...entities.schemas import EntityIntermediate
 from ...schemas import Creator, Infostar
 from ...schemas.attribute import Attribute
 from ...settings import Settings
-from ...utils import creation, reading
 from .token import parse_token, sanitize_client_name, validate_token_against_db
 
 EmailStr = str
 
 
 class RequestAuthenticator:
+    CACHE: dict[str, tuple[Creator, Infostar]] = {}
+
     @classmethod
     def validate(
         cls,
         request: Request,
         user_email: str | None,
         api_key_header: str,
+        background_tasks: BackgroundTasks,
     ):
-        creator, token_creator_user_email = cls.get_request_creator(
-            api_key_header, user_email
-        )
-        infostar = cls.get_request_infostar(creator, request)
-        # TODO: create user entity in DB
-        cls.create_user_on_db(creator, infostar, token_creator_user_email)
+        key = f"user_email={user_email}&api_key_header={api_key_header}"
+        cache_result = cls.CACHE.get(key)
+        if cache_result:
+            creator, infostar = cache_result
+        else:
+            creator, token_creator_user_email = cls.get_request_creator(
+                token=api_key_header,
+                user_email=user_email,
+            )
+            infostar = cls.get_request_infostar(creator)
+            update_callback = cls.verify_user_on_db(
+                creator=creator,
+                infostar=infostar,
+                token_creator_email=token_creator_user_email,
+            )
+            background_tasks.add_task(update_callback)
+            cls.CACHE[key] = (creator, infostar)
 
-        if request.client is not None:
-            creator.user_ip = request.client.host
         if request.headers.get("x-forwarded-for"):
             creator.user_ip = request.headers["x-forwarded-for"]
+        elif request.client is not None:
+            creator.user_ip = request.client.host
+
         request.state.creator = creator
         request.state.infostar = infostar
 
@@ -52,63 +66,7 @@ class RequestAuthenticator:
         return client, name, value
 
     @staticmethod
-    def get_organization_for_client(creator: Creator, infostar: Infostar) -> EntityDAO:
-        """Return the organization entity for a given client name."""
-        if creator.client_name == "/":
-            logger.debug(f"Using root token, org = /.")
-            organization = "/"
-            org_i = EntityIntermediate(
-                handle="/",
-                owner_ref=None,
-                type="organization",
-            )
-            try:  # TODO read or create
-                org = creation.create_one(org_i, EntityDAO, infostar=infostar)
-                logger.debug(f"Created root organization entity.")
-            except HTTPException as e:
-                if e.status_code != 409:
-                    raise e
-        logger.debug(f"Getting organization for client {creator.client_name!r}.")
-        org_handle = creator.client_name.split("/")[1]
-        organization = f"/{org_handle}"
-        filters_org = {"type": "organization", "handle": organization}
-        org = reading.read_one_filters(
-            infostar=infostar, model=EntityDAO, **filters_org
-        )
-        return org
-
-    @staticmethod
-    def get_authprovider(
-        organization: EntityDAO, creator: Creator, infostar: Infostar
-    ) -> AuthProviderDAO:
-        """Return the 'melt-key' AuthProvider for the given client name."""
-        logger.debug(f"Getting 'melt-key' provider for org {organization.handle!r}.")
-        if organization.handle == "/":
-            provider_i = AuthProviderMoreIn(
-                organization_ref=OrganizationRef(**organization.model_dump()),
-                type="melt-key",
-                service_ref=None,
-                extra=[Attribute(name="melt_key_client", value="/")],
-            )
-            try:  # TODO read or create
-                provider = creation.create_one(
-                    provider_i, AuthProviderDAO, infostar=infostar
-                )
-                logger.debug(
-                    f"Created 'melt-key' provider for org {organization.handle!r}."
-                )
-            except HTTPException as e:
-                if e.status_code != 409:
-                    raise e
-        filters = {
-            "type": "melt-key",
-            "organization_ref.handle": organization.handle,
-        }
-        provider = reading.read_one_filters(infostar, model=AuthProviderDAO, **filters)
-        return provider
-
-    @staticmethod
-    def get_request_infostar(creator: Creator, request: Request):
+    def get_request_infostar(creator: Creator):
         logger.debug("Assembling Infostar based on Creator.")
         breadcrumbs = creator.client_name.split("/")
         owner_handle = f"/{breadcrumbs[1]}"
@@ -139,6 +97,13 @@ class RequestAuthenticator:
         cls, token: str, user_email: Optional[str]
     ) -> tuple[Creator, Optional[EmailStr]]:
         """Returns the Creator and token creator user email for a given request."""
+        if user_email is not None:
+            try:
+                validate_email(user_email)
+            except:
+                code, m = s.HTTP_401_UNAUTHORIZED, "User email is not valid."
+                raise HTTPException(status_code=code, detail=m)
+
         logger.debug("Getting Creator for request.")
         client_name, token_name, token_value = cls.get_token_details(token)
         token_creator_user_email = None
@@ -154,24 +119,15 @@ class RequestAuthenticator:
                 print(token_value, Settings().TAUTH_ROOT_API_KEY)
                 code, m = s.HTTP_401_UNAUTHORIZED, "Root token does not match env var."
                 raise HTTPException(status_code=code, detail=m)
-            try:
-                validate_email(user_email)
-            except:
-                code, m = s.HTTP_401_UNAUTHORIZED, "User email is not valid."
-                raise HTTPException(status_code=code, detail=m)
+
             request_creator_user_email = user_email
         else:
             logger.debug("Using non-root token, validating token in DB.")
             token_obj = validate_token_against_db(token, client_name, token_name)
+            token_creator_user_email = token_obj["created_by"]["user_handle"]
             if user_email is None:
                 request_creator_user_email = token_obj["created_by"]["user_handle"]
             else:
-                token_creator_user_email = token_obj["created_by"]["user_handle"]
-                try:
-                    validate_email(user_email)
-                except:
-                    code, m = s.HTTP_401_UNAUTHORIZED, "User email is not valid."
-                    raise HTTPException(status_code=code, detail=m)
                 request_creator_user_email = user_email
 
         creator = Creator(
@@ -182,16 +138,13 @@ class RequestAuthenticator:
         return creator, token_creator_user_email
 
     @classmethod
-    def create_user_on_db(
+    def verify_user_on_db(
         cls,
         creator: Creator,
         infostar: Infostar,
         token_creator_email: Optional[EmailStr],
-    ):
+    ) -> Callable[[], None]:
         logger.debug("Registering user.")
-        client_org = cls.get_organization_for_client(creator, infostar)
-        authprovider = cls.get_authprovider(client_org, creator, infostar)
-        org_ref = EntityRef(**client_org.model_dump())
         melt_key_client_extra = Attribute(
             name="melt_key_client", value=creator.client_name
         )
@@ -199,39 +152,108 @@ class RequestAuthenticator:
             creator.user_email if token_creator_email is None else token_creator_email
         )
 
-        user_data = None
-        try:
-            logger.debug(
-                f"Checking if user {user_creator_email!r} exists in org {client_org.handle!r}."
+        if creator.client_name == "/":
+            org_in = EntityDAO(
+                handle="/",
+                owner_ref=None,
+                type="organization",
+                created_by=infostar,
             )
-            user_data = reading.read_one_filters(
-                infostar,
-                model=EntityDAO,
-                handle=user_creator_email,
-                owner_ref=org_ref.model_dump(),
+            user_in = EntityDAO(
+                handle=creator.user_email,
+                owner_ref=EntityRef(type="organization", handle="/"),
                 type="user",
+                extra=[melt_key_client_extra],
+                created_by=infostar,
             )
-        except HTTPException as e:
-            if e.status_code != 404:
-                raise e
+            collection = EntityDAO.collection(alias=Settings.get().TAUTH_REDBABY_ALIAS)
+            with contextlib.suppress(BulkWriteError):
+                collection.insert_many(
+                    [org_in.bson(), user_in.bson()],
+                    ordered=False,
+                )
 
-        if user_data:
-            logger.debug(
-                f"User {user_creator_email!r} exists. Adding {creator.client_name!r} client info."
+            provider_in = AuthProviderDAO(
+                organization_ref=OrganizationRef(handle="/"),
+                type="melt-key",
+                service_ref=None,
+                extra=[Attribute(name="melt_key_client", value="/")],
+                created_by=infostar,
             )
+            collection = AuthProviderDAO.collection(
+                alias=Settings.get().TAUTH_REDBABY_ALIAS
+            )
+            with contextlib.suppress(DuplicateKeyError):
+                collection.insert_one(provider_in.bson())
+
+        org_handle = creator.client_name.split("/")[1]
+        pipeline = [
+            {"$match": {"type": "organization", "handle": f"/{org_handle}"}},
+            {
+                "$lookup": {
+                    "from": "authproviders",
+                    "localField": "handle",
+                    "foreignField": "organization_ref.handle",
+                    "as": "authprovider",
+                }
+            },
+            {"$unwind": "$authprovider"},
+            {"$match": {"authprovider.type": "melt-key"}},
+            {
+                "$lookup": {
+                    "from": "entities",
+                    "localField": "handle",
+                    "foreignField": "owner_ref.handle",
+                    "as": "user",
+                }
+            },
+            {"$unwind": "$user"},
+        ]
+
+        collection = EntityDAO.collection(alias=Settings.get().TAUTH_REDBABY_ALIAS)
+        if user_creator_email:
+            pipeline.append(
+                {"$match": {"user.type": "user", "user.handle": user_creator_email}}
+            )
+            results = list(collection.aggregate(pipeline))
+            if not results:
+                d = {
+                    "error": "DocumentNotFound",
+                    "msg": f"Document with org_handle=/{org_handle} and user_handle={user_creator_email} not found. User or Authprovider no registered in organization.",
+                }
+                raise HTTPException(status_code=404, detail=d)
+            if len(results) > 1:
+                d = {
+                    "error": "DocumentNotUnique",
+                    "msg": f"Document with org_handle=/{org_handle} and user_handle={user_creator_email} is not unique.",
+                }
+                raise HTTPException(status_code=409, detail=d)
+        else:
+            pipeline.append({"$match": {"user.type": "user"}})
+            results = list(collection.aggregate(pipeline))
+            if not results:
+                d = {
+                    "error": "DocumentNotFound",
+                    "msg": f"Document with org_handle=/{org_handle} not found. Users or Authprovider no registered in organization.",
+                }
+                raise HTTPException(status_code=404, detail=d)
+            if len(results) > 1:
+                d = {
+                    "error": "DocumentNotUnique",
+                    "msg": f"Document with org_handle=/{org_handle} is not unique. Please provide an e-mail to filter results.",
+                }
+                raise HTTPException(status_code=409, detail=d)
+
+        data = results[0]
+        data.pop("authprovider")
+        user = data.pop("user")
+
+        def callback():
+            logger.debug(f"Adding {creator.client_name!r} client info.")
             entity_coll = EntityDAO.collection(alias=Settings.get().TAUTH_REDBABY_ALIAS)
             entity_coll.update_one(
-                filter={"_id": user_data.id},
+                filter={"_id": user["_id"]},
                 update={"$addToSet": {"extra": melt_key_client_extra.model_dump()}},
             )
-            return
 
-        logger.debug(f"User does not exist. Adding user to DB.")
-        user_i = EntityIntermediate(
-            handle=creator.user_email,
-            owner_ref=org_ref,
-            type="user",
-            extra=[melt_key_client_extra],
-        )
-        user = creation.create_one(user_i, EntityDAO, infostar=infostar)
-        logger.debug(f"Registered {user_creator_email!r} in org {client_org.handle!r}.")
+        return callback
