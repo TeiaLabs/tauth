2899495ed05199a51d005406dead2f08c855896d
Ramiro Nilson Barros <ramiro.nilson.barros@gmail.com>
2024-11-11 15:52:41 -0300

    Update get_context_resources

--- tauth/resource_management/resources/controllers.py
+++ tauth/resource_management/resources/controllers.py
@@ -1,3 +1,5 @@
+from tauth.entities.models import EntityDAO
+from tauth.resource_management.access.models import ResourceAccessDAO
 from tauth.resource_management.resources.models import ResourceDAO
 from tauth.schemas.infostar import Infostar
 from tauth.utils import reading
@@ -20,14 +22,51 @@ def read_many(
     return reading.read_many(infostar=infostar, model=ResourceDAO, **filters)
 
 
-def get_context_resources(entity) -> list[ResourceContext]:
-    resources = read_many(
-        infostar=infostar,
-        service_handle=service_handle,
-        resource_collection=resource_collection,
-    )
+def get_context_resources(
+    infostar: Infostar,
+    entity: EntityDAO,
+    service_handle: str,
+    resource_collection: str,
+) -> list[ResourceContext]:
+    """Get context resources
 
-    permissions = read_permissions_from_roles(roles)
+    Args:
+        infostar (Infostar): Infostar
+        entity (EntityDAO): EntityDAO
+        service_handle (str): service_handle
+        resource_collection (str): resource_collection
+
+    """
+    pipeline = [
+        {"$match": {"entity_ref.handle": entity.handle}},
+        {
+            "$lookup": {
+                "from": "resources",
+                "localField": "resource_id",
+                "foreignField": "_id",
+                "as": "resource_details",
+            }
+        },
+        {"$unwind": "$resource_details"},
+        {
+            "$match": {
+                "resource_details.service_ref.handle": service_handle,
+                "resource_details.resource_collection": resource_collection,
+            }
+        },
+    ]
+
+    resources = reading.aggregate(
+        infostar=infostar,
+        model=ResourceAccessDAO,
+        result_model=ResourceDAO,
+        pipeline=pipeline,
+    )
+    # TODO: Add permissions from entity permission list
+    permissions = read_permissions_from_roles([role.id for role in entity.roles])
+    permissions_list = [
+        context for contexts in permissions.values() for context in contexts
+    ]
 
     resource_context: list[ResourceContext] = []
     for resource in resources:
@@ -36,7 +75,7 @@ def get_context_resources(entity) -> list[ResourceContext]:
             resource_collection=resource.resource_collection,
             ids=resource.ids,
             metadata=resource.metadata,
-            permissions=permissions[resource.role_ref.id],
+            permissions=permissions_list,
         )
         resource_context.append(obj)
 
