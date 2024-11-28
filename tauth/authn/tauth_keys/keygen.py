0c1c185691ee054dda76ebaabd5d2ca86c360099
FHaggs <pedrofamh@protonmail.com>
2024-11-28 17:03:06 -0300

    Remove hashable fields, use ObjectIds for tokenDAO

--- tauth/authn/tauth_keys/keygen.py
+++ tauth/authn/tauth_keys/keygen.py
@@ -1,6 +1,8 @@
 import secrets
 from hashlib import sha256
 
+from redbaby.pyobjectid import PyObjectId
+
 from tauth.schemas.infostar import Infostar
 from tauth.settings import Settings
 
@@ -15,28 +17,30 @@ def hash_value(value: str) -> str:
 
 
 def generate_key_value(
-    token_id: str,
+    token_id: PyObjectId,
 ):
     """
     The tauth key: TAUTH_<db_id>_<secret>
     """
-    return f"TAUTH_{token_id}_{secrets.token_hex(32)}"
+    return f"TAUTH_{str(token_id)}_{secrets.token_hex(32)}"
 
 
 def create(
     dto: TauthTokenCreationIntermidiate, infostar: Infostar
 ) -> tuple[TauthTokenDAO, TauthTokenCreationOut]:
 
+    id = PyObjectId()
+
+    secret = generate_key_value(id)
+
     token_dao = TauthTokenDAO(
+        _id=id,
         name=dto.name,
-        value_hash="mock",
+        value_hash=hash_value(secret),
         roles=dto.roles,
         entity=dto.entity,
         created_by=infostar,
     )
-
-    secret = generate_key_value(token_dao.id)
-    token_dao.value_hash = hash_value(secret)
 
     token_out_dto = TauthTokenCreationOut(value=secret, **dto.model_dump())
 
